add7a9ca24e81b61b6b8c9977a5735322d57da6c
lubia <yanyuan2046@126.com>
2013-10-15 15:43:52 +0800

    fix

--- src/main.go
+++ src/main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
-	"app"
+	"log"
+	"modbus"
+	"os"
 )
 
 func main() {
-	//app.Read()
-	app.Crc16()
-} 
+	fd, err := os.Open("/dev/ttyAM0")
+	if err != nil {
+		log.Println("unable to open rs485")
+		return
+	}
+	b, err := modbus.ModbusRead(fd, 1, 3, 1, []byte{0, 0})
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	log.Println(b)
+	err = modbus.ModbusWrite(fd, 1, 3, 1, []byte{0, 1})
+	if err != nil {
+		log.Println(err.Error())
+	}
+}
